c5c480539f990264e5cd693dbbf2e49103a8ee06
Leif.huan <leif.huan@hi.com>
2024-08-09 01:46:37 +0800

    u

--- src/components/kline/lightweight-charts.standalone.development2.js
+++ src/components/kline/lightweight-charts.standalone.development2.js
@@ -1897,66 +1897,74 @@
     }
 
     // eslint-disable-next-line max-params, complexity
+    // ###-walkLine-###-333
     function walkLine(renderingScope, items, lineType, visibleRange, barWidth, 
     // the values returned by styleGetter are compared using the operator !==,
     // so if styleGetter returns objects, then styleGetter should return the same object for equal styles
     styleGetter, finishStyledArea) {
-        if (items.length === 0 || visibleRange.from >= items.length || visibleRange.to <= 0) {
-            return;
-        }
-        const { context: ctx, horizontalPixelRatio, verticalPixelRatio } = renderingScope;
-        const firstItem = items[visibleRange.from];
-        let currentStyle = styleGetter(renderingScope, firstItem);
-        let currentStyleFirstItem = firstItem;
-        if (visibleRange.to - visibleRange.from < 2) {
-            const halfBarWidth = barWidth / 2;
-            ctx.beginPath();
-            const item1 = { _internal_x: firstItem._internal_x - halfBarWidth, _internal_y: firstItem._internal_y };
-            const item2 = { _internal_x: firstItem._internal_x + halfBarWidth, _internal_y: firstItem._internal_y };
-            ctx.moveTo(item1._internal_x * horizontalPixelRatio, item1._internal_y * verticalPixelRatio);
-            ctx.lineTo(item2._internal_x * horizontalPixelRatio, item2._internal_y * verticalPixelRatio);
-            finishStyledArea(renderingScope, currentStyle, item1, item2);
-        }
-        else {
-            const changeStyle = (newStyle, currentItem) => {
-                finishStyledArea(renderingScope, currentStyle, currentStyleFirstItem, currentItem);
+        let drawLine = () => {
+            if (items.length === 0 || visibleRange.from >= items.length || visibleRange.to <= 0) {
+                return;
+            }
+            const { context: ctx, horizontalPixelRatio, verticalPixelRatio } = renderingScope;
+            const firstItem = items[visibleRange.from];
+            let currentStyle = styleGetter(renderingScope, firstItem);
+            let currentStyleFirstItem = firstItem;
+            if (visibleRange.to - visibleRange.from < 2) {
+                const halfBarWidth = barWidth / 2;
                 ctx.beginPath();
-                currentStyle = newStyle;
-                currentStyleFirstItem = currentItem;
-            };
-            let currentItem = currentStyleFirstItem;
-            ctx.beginPath();
-            ctx.moveTo(firstItem._internal_x * horizontalPixelRatio, firstItem._internal_y * verticalPixelRatio);
-            for (let i = visibleRange.from + 1; i < visibleRange.to; ++i) {
-                currentItem = items[i];
-                const itemStyle = styleGetter(renderingScope, currentItem);
-                switch (lineType) {
-                    case 0 /* LineType.Simple */:
-                        ctx.lineTo(currentItem._internal_x * horizontalPixelRatio, currentItem._internal_y * verticalPixelRatio);
-                        break;
-                    case 1 /* LineType.WithSteps */:
-                        ctx.lineTo(currentItem._internal_x * horizontalPixelRatio, items[i - 1]._internal_y * verticalPixelRatio);
-                        if (itemStyle !== currentStyle) {
-                            changeStyle(itemStyle, currentItem);
+                const item1 = { _internal_x: firstItem._internal_x - halfBarWidth, _internal_y: firstItem._internal_y };
+                const item2 = { _internal_x: firstItem._internal_x + halfBarWidth, _internal_y: firstItem._internal_y };
+                ctx.moveTo(item1._internal_x * horizontalPixelRatio, item1._internal_y * verticalPixelRatio);
+                ctx.lineTo(item2._internal_x * horizontalPixelRatio, item2._internal_y * verticalPixelRatio);
+                finishStyledArea(renderingScope, currentStyle, item1, item2);
+            } else {
+                const changeStyle = (newStyle, currentItem) => {
+                    finishStyledArea(renderingScope, currentStyle, currentStyleFirstItem, currentItem);
+                    ctx.beginPath();
+                    currentStyle = newStyle;
+                    currentStyleFirstItem = currentItem;
+                };
+                let currentItem = currentStyleFirstItem;
+                ctx.beginPath();
+                ctx.moveTo(firstItem._internal_x * horizontalPixelRatio, firstItem._internal_y * verticalPixelRatio);
+                
+                for (let i = visibleRange.from + 1; i < visibleRange.to; ++i) {
+                    currentItem = items[i];
+                    const itemStyle = styleGetter(renderingScope, currentItem);
+                    switch (lineType) {
+                        case 0 /* LineType.Simple */:
+                            ctx.lineTo(currentItem._internal_x * horizontalPixelRatio, currentItem._internal_y * verticalPixelRatio);
+                            break;
+                        case 1 /* LineType.WithSteps */:
                             ctx.lineTo(currentItem._internal_x * horizontalPixelRatio, items[i - 1]._internal_y * verticalPixelRatio);
+                            if (itemStyle !== currentStyle) {
+                                changeStyle(itemStyle, currentItem);
+                                ctx.lineTo(currentItem._internal_x * horizontalPixelRatio, items[i - 1]._internal_y * verticalPixelRatio);
+                            }
+                            ctx.lineTo(currentItem._internal_x * horizontalPixelRatio, currentItem._internal_y * verticalPixelRatio);
+                            break;
+                        case 2 /* LineType.Curved */: {
+                            const [cp1, cp2] = getControlPoints(items, i - 1, i);
+                            ctx.bezierCurveTo(cp1._internal_x * horizontalPixelRatio, cp1._internal_y * verticalPixelRatio, cp2._internal_x * horizontalPixelRatio, cp2._internal_y * verticalPixelRatio, currentItem._internal_x * horizontalPixelRatio, currentItem._internal_y * verticalPixelRatio);
+                            break;
                         }
-                        ctx.lineTo(currentItem._internal_x * horizontalPixelRatio, currentItem._internal_y * verticalPixelRatio);
-                        break;
-                    case 2 /* LineType.Curved */: {
-                        const [cp1, cp2] = getControlPoints(items, i - 1, i);
-                        ctx.bezierCurveTo(cp1._internal_x * horizontalPixelRatio, cp1._internal_y * verticalPixelRatio, cp2._internal_x * horizontalPixelRatio, cp2._internal_y * verticalPixelRatio, currentItem._internal_x * horizontalPixelRatio, currentItem._internal_y * verticalPixelRatio);
-                        break;
+                    }
+                    if (lineType !== 1 && itemStyle !== currentStyle) {
+                        changeStyle(itemStyle, currentItem);
+                        ctx.moveTo(currentItem._internal_x * horizontalPixelRatio, currentItem._internal_y * verticalPixelRatio);
                     }
                 }
-                if (lineType !== 1 /* LineType.WithSteps */ && itemStyle !== currentStyle) {
-                    changeStyle(itemStyle, currentItem);
-                    ctx.moveTo(currentItem._internal_x * horizontalPixelRatio, currentItem._internal_y * verticalPixelRatio);
+                if (currentStyleFirstItem !== currentItem || currentStyleFirstItem === currentItem && lineType === 1 /* LineType.WithSteps */) {
+                    finishStyledArea(renderingScope, currentStyle, currentStyleFirstItem, currentItem);
                 }
             }
-            if (currentStyleFirstItem !== currentItem || currentStyleFirstItem === currentItem && lineType === 1 /* LineType.WithSteps */) {
-                finishStyledArea(renderingScope, currentStyle, currentStyleFirstItem, currentItem);
-            }
+        };
+        let run = () => {
+            drawLine();
+            requestAnimationFrame(run);
         }
+       run();
     }
     const curveTension = 6;
     function subtract(p1, p2) {
@@ -1978,7 +1986,7 @@
         const cp2 = subtract(points[toPointIndex], divide(subtract(points[afterToPointIndex], points[fromPointIndex]), curveTension));
         return [cp1, cp2];
     }
-
+    // ###-555
     function finishStyledArea$1(baseLevelCoordinate, scope, style, areaFirstItem, newAreaFirstItem) {
         const { context, horizontalPixelRatio, verticalPixelRatio } = scope;
         context.lineTo(newAreaFirstItem._internal_x * horizontalPixelRatio, baseLevelCoordinate * verticalPixelRatio);
@@ -2136,6 +2144,7 @@
         ctx.strokeStyle = style;
         ctx.stroke();
     }
+    // ###-222
     class PaneRendererLineBase extends BitmapCoordinatesPaneRenderer {
         constructor() {
             super(...arguments);
@@ -2159,6 +2168,7 @@
             ctx.lineJoin = 'round';
             const styleGetter = this._internal__strokeStyle.bind(this);
             if (lineType !== undefined) {
+                // console.log("#items ", items);
                 walkLine(renderingScope, items, lineType, visibleRange, barWidth, styleGetter, finishStyledArea);
             }
             if (pointMarkersRadius) {
@@ -2169,7 +2179,7 @@
 
     class PaneRendererLine extends PaneRendererLineBase {
         _internal__strokeStyle(renderingScope, item) {
-            return item._internal_lineColor;
+           return item._internal_lineColor;
         }
     }
 
@@ -2264,8 +2274,8 @@
                 this._internal__optionsInvalidated = false;
             }
             if (this._internal__invalidated) {
-                this._private__makeValidImpl();
                 this._internal__invalidated = false;
+                this._private__makeValidImpl();
             }
         }
         _private__makeValidImpl() {
@@ -2288,6 +2298,8 @@
             }
             this._internal__itemsVisibleRange = visibleTimedValues(this._internal__items, visibleBars, this._private__extendedVisibleRange);
             this._internal__convertToCoordinates(priceScale, timeScale, firstValue._internal_value);
+            
+            // ###-0000  stark line
             this._internal__prepareRendererData();
         }
     }
@@ -2316,7 +2328,7 @@
             });
         }
     }
-
+    // ###-111
     class SeriesAreaPaneView extends LinePaneViewBase {
         constructor(series, model) {
             super(series, model);
@@ -2330,6 +2342,7 @@
         }
         _internal__prepareRendererData() {
             const options = this._internal__series._internal_options();
+            // ### area
             this._private__areaRenderer._internal_setData({
                 _internal_lineType: options.lineType,
                 _internal_items: this._internal__items,
@@ -5651,7 +5664,7 @@
                 }
                 const invCoordinate = bh + hmm * (logical - min);
                 const coordinate = isInverted ? invCoordinate : this._private__height - 1 - invCoordinate;
-                point._internal_y = coordinate;
+                point._internal_y = coordinate; // ###-y
             }
         }
         _internal_barPricesToCoordinates(pricesList, baseValue, visibleRange) {
@@ -6805,7 +6818,7 @@
             }
             // updating bar spacing should be first because right offset depends on it
             this._private__correctBarSpacing();
-            this._private__correctOffset();
+            // this._private__correctOffset();
         }
         _internal_indexToCoordinate(index) {
             if (this._internal_isEmpty() || !isInteger(index)) {
@@ -6824,7 +6837,7 @@
                 const index = points[i]._internal_time;
                 const deltaFromRight = baseIndex + this._private__rightOffset - index;
                 const coordinate = this._private__width - (deltaFromRight + 0.5) * this._private__barSpacing - 1;
-                points[i]._internal_x = coordinate;
+                points[i]._internal_x = coordinate; // ###-x
             }
         }
         _internal_coordinateToIndex(x) {
@@ -7844,7 +7857,7 @@
             percent = Math.max(0, Math.min(100, Math.round(percent * 100)));
             if (this._private__gradientColorsCache === null ||
                 this._private__gradientColorsCache._internal_topColor !== topColor || this._private__gradientColorsCache._internal_bottomColor !== bottomColor) {
-                this._private__gradientColorsCache = {
+                    this._private__gradientColorsCache = {
                     _internal_topColor: topColor,
                     _internal_bottomColor: bottomColor,
                     _internal_colors: new Map(),
